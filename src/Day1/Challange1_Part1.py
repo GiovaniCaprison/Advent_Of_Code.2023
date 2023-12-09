d1d20df299f7cb5cf12b690ad0caa699f40d140d
Louis Grennell <icantfindanadressname@gmail.com>
2023-12-09 13:22:06 +0000

    Solution

--- src/Day1/Challange1_Part1.py
+++ src/Day1/Challange1_Part1.py
@@ -2,7 +2,6 @@ import re
 
 file_path = '/example/file/path.txt'
 
-# Reading the file
 with open(file_path, 'r') as file:
     calibration_data = file.readlines()
 
@@ -10,15 +9,12 @@ with open(file_path, 'r') as file:
 def calculate_calibration_sum(data):
     total_sum = 0
     for line in data:
-        # Extracting digits from each line
         digits = re.findall(r'\d', line)
         if digits:
-            # Combining the first and last digit to form a two-digit number
             calibration_value = int(digits[0] + digits[-1])
             total_sum += calibration_value
     return total_sum
 
 
-# Calculating the total sum of calibration values
 total_calibration_sum = calculate_calibration_sum(calibration_data)
 print(total_calibration_sum)
