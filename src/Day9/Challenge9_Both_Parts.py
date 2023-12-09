63fe24e2ad05aacf2e284b0fe1903f4228f0b372
Louis Grennell <icantfindanadressname@gmail.com>
2023-12-09 15:05:32 +0000

    Solution

--- src/Day9/Challenge9_Both_Parts.py
+++ src/Day9/Challenge9_Both_Parts.py
@@ -1,11 +1,11 @@
-def next_val(x, d='next'):
+def ans(x, d='n'):
     s = [x]
 
     while not all(v == 0 for v in s[-1]):
         ns = [s[-1][i + 1] - s[-1][i] for i in range(len(s[-1]) - 1)]
         s.append(ns)
 
-    if d == 'next':
+    if d == 'n':
         for i in range(len(s) - 2, -1, -1):
             s[i].append(s[i][-1] + s[i + 1][-1])
         return s[0][-1]
@@ -23,7 +23,7 @@ with open(fp, 'r') as file:
 
 h = [list(map(int, line.strip().split())) for line in h]
 
-ans1 = sum(next_val(line, 'next') for line in h)
-ans2 = sum(next_val(line, 'prev') for line in h)
+ans1 = sum(ans(line, 'n') for line in h)
+ans2 = sum(ans(line, 'p') for line in h)
 
 print(ans1, ans2)
